786497da7ea61dfeb312e98f1ced28d13c7b0d3b
Liam Silverman <liamsilvm@gmail.com>
2023-03-16 19:35:22 +0000

    Updates

---
+++ mapsterCoords.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('#image').mapster({
+        fill: true, 
+        fillColor: '32a852',
+        singleSelect:true
+    //   singleSelect: true, 
+    //   fill : true, 
+    //   fillOpacity: 0.6, 
+    //   fillColor: '32a852'
+    
+})
+
+  });
+
+addEventListener("resize", (event) => { 
+    $(document).ready(function() {
+        $('#image').mapster({
+            fill: true, 
+            fillColor: '32a852',
+            singleSelect:true
+        //   singleSelect: true, 
+        //   fill : true, 
+        //   fillOpacity: 0.6, 
+        //   fillColor: '32a852'
+        })
+      });
+})
+
+
+
+
